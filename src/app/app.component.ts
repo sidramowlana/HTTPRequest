a85b96264c6e36c024a1b8c52d34111169d4a6fa
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-10-19 00:45:49 +0530

    setting up the formatof the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project Http Request';
+  firbaseLink="https://http-request-project-661a3.firebaseio.com/";
+
+  servers=[
+    {
+      name:'TestServer',
+      capacity:100,
+      serverID:this.generateId()
+    },
+    {
+      name:'My Server',
+      capacity:10,
+      serverID:this.generateId()
+    },
+    {
+      name:'Live Server',
+      capacity:30,
+      serverID:this.generateId()
+    }    
+  ]
+
+  onAddServer(serverName:string){
+  this.servers.push({
+    name:name,
+    capacity:50,
+    serverID:this.generateId()
+  })
+  }
+  generateId(){
+    return Math.round(Math.random()*1000);
+  }
 }
